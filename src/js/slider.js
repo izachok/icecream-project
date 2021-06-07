2acb4f32e57ebb39d3fa8ae9efc48060e9f6035e
Taras-Kolomiiets <taraskolomiiets8991@gmail.com>
2021-06-07 18:53:10 +0300

    MVP slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,27 +1,13 @@
-/* Индекс слайда по умолчанию */
+ /* Индекс слайда по умолчанию */
 var slideIndex = 1;
 showSlides(slideIndex);
-
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
-function plusSlide() {
-    showSlides(slideIndex += 1);
-}
-
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-function minusSlide() {
-    showSlides(slideIndex -= 1);  
-}
-
-/* Устанавливает текущий слайд */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
 
 /* Основная функция слайдера */
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("item");
     var dots = document.getElementsByClassName("slider-dots_item");
+    var text = document.getElementsByClassName("customer-reviews__text");
     if (n > slides.length) {
       slideIndex = 1
     }
@@ -30,10 +16,30 @@ function showSlides(n) {
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
+        text[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
+    text[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+var listElements = document.querySelectorAll('.dots');
+var sliderNext = document.querySelector('.sliderNext');
+var sliderPrev = document.querySelector('.sliderPrev');
+
+listElements.forEach(function(item) {
+  item.addEventListener('click', function(event) {
+    showSlides(slideIndex = event.currentTarget.getAttribute('data-number'))
+  })
+})
+
+sliderNext.addEventListener('click', function(event) {
+  showSlides(slideIndex = slideIndex + 1)
+})
+
+sliderPrev.addEventListener('click', function(event) {
+  showSlides(slideIndex = slideIndex - 1)
+})
