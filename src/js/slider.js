21c548efc5d511ae970ac25135e12747d71adc3b
Olga Tiutiunnyk <izachok@gmail.com>
2021-06-07 22:33:09 +0300

    fix dots for slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,45 +2,53 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slider__item");
-    var dots = document.getElementsByClassName("slider-dots__item");
-    var text = document.getElementsByClassName("customer-reviews__text");
-    var address = document.getElementsByClassName("customer-reviews__address");
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        text[i].style.display = "none";
-       address[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    text[slideIndex - 1].style.display = "block";
-    address[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+  var i;
+  var sliderContainer = document.querySelector('#customer-reviews');
+
+  var slides = sliderContainer.querySelectorAll('.slider__item');
+  var dots = sliderContainer.querySelectorAll('.slider-dots__item');
+  var text = sliderContainer.querySelectorAll('.customer-reviews__text');
+  var address = sliderContainer.querySelectorAll('.customer-reviews__address');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+    text[i].style.display = 'none';
+    address[i].style.display = 'none';
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active');
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+  text[slideIndex - 1].style.display = 'block';
+  address[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('active');
 }
 
-var listElements = document.querySelectorAll('.dots');
+var listElements = document.querySelectorAll('.slider-dots__item');
+
 var sliderNext = document.querySelector('.sliderNext');
 var sliderPrev = document.querySelector('.sliderPrev');
 
-listElements.forEach(function(item) {
-  item.addEventListener('click', function(event) {
-    showSlides(slideIndex = event.currentTarget.getAttribute('data-number'))
-  })
-})
+listElements.forEach(function (item) {
+  item.addEventListener('click', function (event) {
+    showSlides((slideIndex = event.currentTarget.getAttribute('data-number')));
+  });
+});
 
-sliderNext.addEventListener('click', function(event) {
-  showSlides(slideIndex = slideIndex + 1)
-})
+sliderNext.addEventListener('click', function (event) {
+  showSlides((slideIndex = slideIndex + 1));
+});
 
-sliderPrev.addEventListener('click', function(event) {
-  showSlides(slideIndex = slideIndex - 1)
-})
+sliderPrev.addEventListener('click', function (event) {
+  showSlides((slideIndex = slideIndex - 1));
+});
